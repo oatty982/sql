bf5e0033289068b718f223aa419968e81446383b
Patcharapon Chaiporn <oatpatcharapon01@hotmail.com>
2023-06-23 18:11:35 +0700

    sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO orders (order_id, order_date, order_customer_id, order_status)
+VALUES (1,'2013-07-25 00:00:00',11599,'CLOSED');
+INSERT INTO orders (order_id, order_date, order_customer_id, order_status)
+VALUES (2,'2013-07-25 00:00:00',256,'PENDING_PAYMENT');
+INSERT INTO orders (order_id, order_date, order_customer_id, order_status)
+VALUES (3,'2013-07-25 00:00:00',12111,'COMPLETE');
+
+INSERT INTO orders (order_id, order_date, order_customer_id, order_status)
+VALUES 
+    (1,'2013-07-25 00:00:00',11599,'CLOSED'),
+    (2,'2013-07-25 00:00:00',256,'PENDING_PAYMENT'),
+    (3,'2013-07-25 00:00:00',12111,'COMPLETE');
